6abbc7493124883948db8dd0da85f960f8858dac
Michael Nickey <mnickey@gmail.com>
2015-12-26 12:50:45 -0800

    added store app view and tempaltes as well as url speration

--- django_bookstore/urls.py
+++ django_bookstore/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'django_bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # url(r'^$', 'store.views.index', name='index'),
     url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
